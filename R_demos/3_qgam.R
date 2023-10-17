24f9c8bebf48495f0acff59b905ccc4edef8a3eb
Matteo <matteo.fasiolo@gmail.com>
2023-10-17 22:54:43 +0100

    Update qgam

--- R_demos/3_qgam.R
+++ R_demos/3_qgam.R
@@ -50,7 +50,7 @@ summary(fit09_2)
 fit09_2 <- getViz(fit09_2, nsim = 100) # Note nsim does not make sense here! 
 
 # or using shortcut
-fit09_2 <- qgamV(list(accel ~ s(times, k=20, bs="ad"),                             ~ s(times)),
+fit09_2 <- qgamV(list(accel ~ s(times, k=20, bs="ad"), ~ s(times)),
                  data = mcycle,
                  qu = 0.9)
 
@@ -59,12 +59,6 @@ fitMQ <- mqgam(list(accel ~ s(times, k=20, bs="ad"),
                           ~ s(times)),
                data = mcycle, 
                qu = c(0.1, 0.25, 0.5, 0.75, 0.9)) 
-
-# This should be manipulated using the `qdo` function
-qdo(fitMQ, 0.1, summary)
-
-# To plot one of the fitted quantile models we do
-qdo(fitMQ, 0.1, plot)
 
 # To plot effect of several quantile at once we use `mgcViz`
 fitMQ_viz <- getViz(fitMQ)
@@ -76,14 +70,6 @@ for(ii in 1:5){
   lines(mcycle$times, predict(fitMQ_viz[[ii]]), col = 2)
 }
 
-# NB output of `getViz` is simply a list of `gam` models, 
-# we don't need to use `qdo`. 
-# But there is a memory price to pay (small for this small data set)
-format(object.size(fitMQ), units = "MB")
-
-format(object.size(fitMQ_viz), units = "MB")
-
-
 ######################################################
 ###### [2] UK load modelling example
 ######################################################
@@ -92,19 +78,19 @@ format(object.size(fitMQ_viz), units = "MB")
 library(mgcViz);
 data("UKload")
 
-head(UKload)
+head(UKload)[ , c("NetDemand", "NetDemand.48", "Year", "Posan", 
+                  "Dow", "Trend", "wM", "wM_s95", "Holy")]
 
 # Fit basic QGAM and plot smooths:
 qu <- 0.5
-form <- NetDemand~s(wM) + s(wM_s95) + 
-        s(Posan, k = 30, bs = "ad") + Dow + s(Trend,k=4) + 
+form <- NetDemand ~ s(wM) + s(wM_s95) + 
+        s(Posan, k = 30, bs = "ad") + Dow + s(Trend, k=4) + 
         NetDemand.48 + Holy
 fit <- qgamV(form = form, data = UKload, qu = qu)
 
-# 
 print(plot(fit), pages = 1)
 
-check1D(fit, x = "Posan") + l_gridCheck1D(mad)
+check1D(fit, x = "Posan") + l_gridCheck1D(sd, n = 40)
 
 # Add adaptive smooth for Posan and re-plot:
 form <- list(NetDemand ~ s(wM) + s(wM_s95) + s(Posan, bs='ad', k=30) + 
@@ -116,15 +102,10 @@ nqu <- 5
 qus <- seq(0.1, 0.9, length.out = nqu)
 fitM <- mqgamV(form = form, data = UKload, qu = qus)
 
-print(plot(fitM), pages = 1)
-
-# We can also look at the parametric effects:
-print(plot(fitM, allTerms = T, select = 5:7), pages = 1)
+print(plot(fitM, allTerms = T), pages = 2)
 # It is interesting to notice that the holiday effect is stronger 
 # (more negative) on the low quantiles.  
 
 # We can check the fit:
-pl <- check1D(fitM[[3]], 
-              x = list("Dow", "wM", "wM_s95", "Posan")) + l_gridQCheck1D(qu = qus[3]) 
-print(pl, pages = 1)
+check1D(fitM[[3]], x = "wM") + l_gridQCheck1D(qu = qus[3]) 
 
